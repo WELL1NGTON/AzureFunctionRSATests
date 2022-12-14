63025d32608953fc698f74f22aafb948c0fff64e
Wellington Carlos Massola <wellingtonmassola@outlook.com>
2022-12-13 21:17:15 -0300

    fix: fixed incorrect startup
    Added attribute assembly with FunctionStartup.
    
    Fixed wrong environment variable name.

--- src/AzureFunctionRSATests.API/Startup.cs
+++ src/AzureFunctionRSATests.API/Startup.cs
@@ -4,14 +4,15 @@ using System.Text;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 
+[assembly: FunctionsStartup(typeof(AzureFunctionRSATests.API.Startup))]
 namespace AzureFunctionRSATests.API;
 
 public class Startup : FunctionsStartup
 {
     public override void Configure(IFunctionsHostBuilder builder)
     {
-        var rsa = Base64ToRSA(Environment.GetEnvironmentVariable("RSA_KEY") ?? throw new InvalidOperationException("RSA_KEY is not set"));
-        builder.Services.AddSingleton(rsa);
+        var rsa = Base64ToRSA(Environment.GetEnvironmentVariable("RSA_KEY_BASE64") ?? throw new InvalidOperationException("RSA_KEY_BASE64 is not set"));
+        builder.Services.AddSingleton(new TestClassWithRSA { RSA = rsa });
     }
 
     private static RSA Base64ToRSA(string base64)
